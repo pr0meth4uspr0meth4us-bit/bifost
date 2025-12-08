4a0ebd43b44f94e64a5695a5cf62dc9899ca140f
office <phearaniron.soeung@gmail.com>
2025-12-08 09:13:48 +0700

    otp

--- config.py
+++ config.py
@@ -11,9 +11,9 @@ class Config:
 
     # --- EMAIL SETTINGS (NEW) ---
     EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-    SMTP_SERVER = os.environ.get('SMTP_SERVER', 'smtp.mail.me.com')
+    SMTP_SERVER = os.environ.get('SMTP_SERVER', 'smtp.gmail.com')
     SMTP_PORT = int(os.environ.get('SMTP_PORT', 587))
-    SENDER_EMAIL = os.environ.get('SENDER_EMAIL', 'bifrost.helm@icloud.com')
+    SENDER_EMAIL = 'bifrostbyhelm@gmail.com'
 
     if not SECRET_KEY or not MONGO_URI or not JWT_SECRET_KEY or not EMAIL_PASSWORD:
         raise RuntimeError("CRITICAL: Missing .env keys (EMAIL_PASSWORD, SECRET_KEY, etc.)")
